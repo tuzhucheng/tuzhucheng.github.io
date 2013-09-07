03fc7c7aaf266f0cbe6a2b6ef9e5f69005bbb0f7
Michael Tu <tuzhucheng@outlook.com>
2013-09-07 18:07:08 -0400

    Added GitHub, LinkedIn, and email badges to navigation, with color transition using jQuery Color

--- js/animations.js
+++ js/animations.js
@@ -1,3 +1,23 @@
 $(document).ready(function() {
-	$("#introduction").animate({"margin-top": "+=150px" }, 800);     
+	$("#introduction").animate({"margin-top": "+=150px" }, 800);
+	
+	// Change background of badges on hover
+	
+	$("#github_logo").hover(function() {
+			$(this).animate({backgroundColor: jQuery.Color("rgb(65,131,196)")}, 200);
+		}, function() {
+			$(this).animate({backgroundColor: jQuery.Color("rgba(38,38,38,0.8)")}, 200);
+	});
+	
+	$("#linkedin_logo").hover(function() {
+			$(this).animate({backgroundColor: jQuery.Color("rgb(0,127,177)")}, 200);
+		}, function() {
+			$(this).animate({backgroundColor: jQuery.Color("rgba(38,38,38,0.8)")}, 200);
+	});
+
+	$("#email_logo").hover(function() {
+			$(this).animate({backgroundColor: jQuery.Color("rgb(255,153,0)")}, 200);
+		}, function() {
+			$(this).animate({backgroundColor: jQuery.Color("rgba(38,38,38,0.8)")}, 200);
+	});       
 });
